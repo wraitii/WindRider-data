188a707b5d4284728698cafd848aed3b45c0846f
Lancelot de Ferrière <wraitii@gmail.com>
2019-09-26 22:10:58 +0200

    systems -> sectors and a bunch of small updates

--- art/landables/Landable.gd
+++ art/landables/Landable.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
-signal body_entered
+#signal body_entered
     
 func _Body_Entered(body):
-	emit_signal("body_entered", body);
+	#emit_signal("body_entered", body);
+	get_parent().on_body_entered(body);
