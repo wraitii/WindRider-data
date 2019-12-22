26fcdf6b5063ac25d6994dfd1f8ffb12e6407644
Lancelot de Ferrière <wraitii@gmail.com>
2019-12-22 21:58:35 +0100

    revisit jumpzone positions, add a nearby system with fairly remote sectors, make the player start in the most remote

--- missions/NewGame.gd
+++ missions/NewGame.gd
@@ -27,11 +27,33 @@ func _continue(_a):
 	var playerShip = Core.outsideWorldSim.create_resource({'model': 'Starbridge'})
 	Core.gameState.player.ship = playerShip
 	
-	Core.gameState.playerShip.teleport('Kerguelen', Vector3(-2000, 0, 0))
+	# Loads the scene and the sector
+	Core.gameState.playerShip.teleport('Abysseus', Vector3(-2000, 0, 0))
+	
+	# Clear out the sector
+	var ships = Core.outsideWorldSim.get_ships_in('Abysseus')
+	for shipID in ships:
+		print(shipID)
+		if shipID == Core.gameState.playerShip.ID:
+			continue
+		else:
+			var ship = Core.outsideWorldSim.ship(shipID)
+			Core.outsideWorldSim.destroy_ship(ship)
 	
 	var sc = EventPopup.instance()
-	sc.get_node('Title').text = "Let's Go'"
-	sc.get_node('Text').text = "Use key arrows to move around. Use Z to thrust. Use Space to fire."
+	sc.get_node('Title').text = "A strange Noise"
+	sc.get_node('Text').text = """
+	Your sight is hazy. Your mind as well, now that you think of it.
+	\"Where am I?\", you hear yourself think. Of course, you know where you are.
+	You're in your ship... Let me leave you a few instants to collect your thoughts.
+	What day is it, you wonder. That's easy. It's only very far from the last day you were wake.
+	Eight hundred years in cryosleep, give or take a millenia, I guess.
+	You don't remember shutting yourself down.
+	
+	Was there maybe some kind of accident? You suppose it doesn't matter, and I'd be inclined to agree. Your Ship, your old solid' ______, held up. So long.
+
+	For a fleeting moment, your mind drifts to your location. You know exactly who you are, true, but does that really matter, when no-one else knows who you were?
+"""
 	
 	sc.connect("event_ok_pressed", self, "_clean", [sc])
 	sc.anchor_bottom = 0.8
